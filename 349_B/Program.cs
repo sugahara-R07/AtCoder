0329c8d80f93a3d88ecc8f6cecf53556a3c857a6
SR-07 <dc.s7.charlotte@gmail.com>
2024-05-19 03:12:45 +0900

    5/19　ABの過去問を解いた

---
+++ 349_B/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _349_B
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string S = Console.ReadLine();
+            List<Char> list = new List<Char>();
+            foreach (char c in S)
+            {
+                list.Add(c);
+            }
+            if(list.Count == 1)
+            {
+                Console.WriteLine("No");
+                return;
+            }
+                
+
+
+            list.Sort();
+            int a = 1;
+
+            List<int> ans = new List<int>();
+
+            for (int i = 0; i < list.Count -1; i++)
+            {
+                if (list[i] != list[i + 1])
+                {
+                    ans.Add(a);
+                    a = 1;
+                }
+                else
+                {
+                    a++;
+                }
+
+                if (i == list.Count - 2)
+                {
+                    ans.Add(a);
+                }
+            }
+
+            ans.Sort();
+
+            int num = ans[0];
+            int count = 0;
+            for(int i = 0; i< ans.Count; i++)
+            {
+                foreach(var n in ans) 
+                {
+                    if(n == ans[i])
+                        count++;
+                }
+
+                if (count > 2 || count == 1 )
+                {
+                    Console.WriteLine("No");
+                    return;
+                }
+
+                count = 0;
+            }
+
+            Console.WriteLine("Yes");
+
+            Console.ReadLine();
+
+        }
+
+
+    }
+}
---
+++ 374_A/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _347_A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] NK = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int N = NK[0];
+            int K = NK[1];
+            int[] nums = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            List<int> ans = new List<int>();
+            for(int i = 0;i<N; i++)
+            {
+                if (nums[i] % K == 0)
+                {
+                    ans.Add(nums[i] /K);
+                }
+            }
+
+            for(int i = 0; i< ans.Count; i++)
+            {
+                Console.Write(ans[i]);
+                if(i != ans.Count - 1)
+                {
+                    Console.Write(' ');
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
